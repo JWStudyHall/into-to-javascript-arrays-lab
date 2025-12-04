b5f61572a55457fabc41db8763684c8257127e65
JEStudyhall <jaieelizabeth@gmail.com>
2025-12-04 16:37:37 -0500

    setup files

---
+++ app.js
@@ -0,0 +1,30 @@
+// Exercise 1
+const foods = [];
+
+ console.log('Exercise 1 result:', foods);
+
+// Exercise 2
+foods.push('pizza','cheeseburger')
+
+console.log('Exercise 2 result:', foods);
+
+// Exercise 3
+foods.unshift ('taco');
+
+console.log('Exercise 3 result:', foods);
+
+// Exercise 4
+console.log (foods[1])
+let favFood= foods[1]
+console.log('Exercise 4 result:', favFood);
+
+// Exercise 5
+foods[2]= 'tofu'
+foods[3]= 'cheeseburger'
+console.log('Exercise 5 result:', foods);
+
+// Exercise 6
+foods.splice (1,1,"sushi", "cupcake")
+
+
+console.log('Exercise 6 result:', foods);
